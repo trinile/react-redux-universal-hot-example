b6e1781bb9cafc91365d941e7b7cb1733cb140cc
Trini Le <trinile@gmail.com>
2017-03-24 19:44:36 -0700

    use Weather container for dummy and logic

--- src/containers/Weather/Weather.js
+++ src/containers/Weather/Weather.js
@@ -1,37 +1,47 @@
-import React, {Component } from 'react';
-// import { connect } from 'react-redux';
-// import { bindActionCreators } from 'redux';
-// import { reduxForm } from 'redux-form';
+import React, {Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
 // import { WeatherButton } from './components';
+import * as weatherActions from '../redux/modules/weather';
 
+@connect(
+  state => ({ weather: state.weather }),
+  weatherActions )
 export default class Weather extends Component {
-  constructor() {
-    super();
+  static propTypes = {
+    weather: PropTypes.string,
+    getWeather: PropTypes.func,
+  }
+  constructor(props) {
+    super(props);
 
     this.state = {
       input: '',
     };
   }
+
   handleChange = (event) => {
     this.setState({
-      input: event.target.value
+      input: event.target.value,
     });
   }
   handleSubmit = (event) => {
     event.preventDefault();
     console.log(this.state.input);
-    // const zipcode = this.state.input;
-
-    // dispatch action of zipcode input
+    // need to handle validation
+    this.props.getWeather(this.state.input);
   }
 
   render() {
+    const { weather } = this.props;
     return (
-      <form>
-        <label>What's teh weather</label>
-        <input onChange={this.handleChange} type="text" value={this.state.input} name="zipcode" placeholder="type in zipcode"/>
-        <button onClick={this.handleSubmit}>click</button>
-      </form>
+      <div>
+        <form>
+          <label>What's teh weather</label>
+          <input onChange={this.handleChange} type="text" value={this.state.input} name="zipcode" placeholder="type in zipcode"/>
+          <button onClick={this.handleSubmit}>click</button>
+        </form>
+        <p>The Weather at this zipcode: {weather} </p>
+      </div>
     );
   }
 }
